b180de51c206d10285db321727d088ad47421c3f
Tolokoban <fabien.petitjean@free.fr>
2019-04-28 15:30:59 +0200

    Updates

---
+++ precache-manifest.455eb85928c33ff66620889d837c1520.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6a0075db0cc40684b8a5209effcfc54",
+    "url": "/boulder-dash/static/media/row-walk.c6a0075d.png"
+  },
+  {
+    "revision": "0525d4b67f728e481e1a",
+    "url": "/boulder-dash/static/css/main.f60ce40f.chunk.css"
+  },
+  {
+    "revision": "134c1caf819c6a73fea1",
+    "url": "/boulder-dash/static/js/runtime~main.4834410b.js"
+  },
+  {
+    "revision": "a884c0a596d0990d9cb9",
+    "url": "/boulder-dash/static/js/2.8c6db941.chunk.js"
+  },
+  {
+    "revision": "52499cb795c88eb72e90f704671e7cf5",
+    "url": "/boulder-dash/static/media/rain.52499cb7.frag"
+  },
+  {
+    "revision": "9b793a212303ad22a297",
+    "url": "/boulder-dash/static/js/3.db53801d.chunk.js"
+  },
+  {
+    "revision": "22b663f11c85a928643df428c0b78bb9",
+    "url": "/boulder-dash/static/media/diam.22b663f1.mp3"
+  },
+  {
+    "revision": "8e36160e4180eb7e4365caa7fde24c1d",
+    "url": "/boulder-dash/static/media/exit.8e36160e.mp3"
+  },
+  {
+    "revision": "7dc2d8e924f1bb3a37cad79655899bbe",
+    "url": "/boulder-dash/static/media/expl.7dc2d8e9.mp3"
+  },
+  {
+    "revision": "6c7f2a4e9838606d36f8352317aef5ff",
+    "url": "/boulder-dash/static/media/music.6c7f2a4e.mp3"
+  },
+  {
+    "revision": "9dfde545e390e1d007af41ac0d7562c8",
+    "url": "/boulder-dash/static/media/rock.9dfde545.mp3"
+  },
+  {
+    "revision": "2b481ee3114400af8fc954cdcab31954",
+    "url": "/boulder-dash/static/media/background.2b481ee3.jpg"
+  },
+  {
+    "revision": "4b96085270630ea07aa9338ec977ee76",
+    "url": "/boulder-dash/static/media/row-boulder.4b960852.png"
+  },
+  {
+    "revision": "fbed7ca614af81825b52520d060cf7bf",
+    "url": "/boulder-dash/static/media/row-diam.fbed7ca6.png"
+  },
+  {
+    "revision": "2e47f96897cc72cd85a37bcc71f406c5",
+    "url": "/boulder-dash/static/media/exit.2e47f968.png"
+  },
+  {
+    "revision": "992fe60c845367d9c40145c60b1957b4",
+    "url": "/boulder-dash/static/media/row-explo.992fe60c.png"
+  },
+  {
+    "revision": "0a555d7a6a9645ad4cf829111f8f8fb6",
+    "url": "/boulder-dash/static/media/ground.0a555d7a.png"
+  },
+  {
+    "revision": "0525d4b67f728e481e1a",
+    "url": "/boulder-dash/static/js/main.9fc129e5.chunk.js"
+  },
+  {
+    "revision": "79be3ac153a41e8cd2bee781b14da572",
+    "url": "/boulder-dash/static/media/row-monster.79be3ac1.png"
+  },
+  {
+    "revision": "bc9ac80319608cc0e491c41342a349bf",
+    "url": "/boulder-dash/static/media/wall.bc9ac803.jpg"
+  },
+  {
+    "revision": "2c988a5242d03b46b6c4bb5da787fcfb",
+    "url": "/boulder-dash/static/media/hue.2c988a52.vert"
+  },
+  {
+    "revision": "c70344c5f45274af9dd0b221476c748e",
+    "url": "/boulder-dash/static/media/hue.c70344c5.frag"
+  },
+  {
+    "revision": "0a667af50169e747937ea3c2bdbea406",
+    "url": "/boulder-dash/static/media/coords.0a667af5.vert"
+  },
+  {
+    "revision": "3655ffb518c01825c6fe8d478ca637d3",
+    "url": "/boulder-dash/static/media/coords-move.3655ffb5.vert"
+  },
+  {
+    "revision": "29fc1d71965b794c3bc5fe2936155885",
+    "url": "/boulder-dash/static/media/transition.29fc1d71.vert"
+  },
+  {
+    "revision": "4c773719edf235b895d0ff2b85d14732",
+    "url": "/boulder-dash/static/media/transition.4c773719.frag"
+  },
+  {
+    "revision": "bf6d184a079aab7917902c9c4323b51e",
+    "url": "/boulder-dash/static/media/background.bf6d184a.vert"
+  },
+  {
+    "revision": "c266c002713189c9a53ec2ed8a7d93f3",
+    "url": "/boulder-dash/static/media/background.c266c002.frag"
+  },
+  {
+    "revision": "ab246fe94544738fd0187ddefd782c1b",
+    "url": "/boulder-dash/static/media/wall.ab246fe9.vert"
+  },
+  {
+    "revision": "b699dc58bc52fd92e99b7c251fa3b1b8",
+    "url": "/boulder-dash/static/media/wall.b699dc58.frag"
+  },
+  {
+    "revision": "273a6aa9a9412ad171f9b89174e2807f",
+    "url": "/boulder-dash/static/media/level.273a6aa9.vert"
+  },
+  {
+    "revision": "dc63b5ff961ad1a170702ccdabc51f14",
+    "url": "/boulder-dash/static/media/level.dc63b5ff.frag"
+  },
+  {
+    "revision": "9c6fc13a838f26ade6aa8963565dcf3b",
+    "url": "/boulder-dash/static/media/rain.9c6fc13a.vert"
+  },
+  {
+    "revision": "9b793a212303ad22a297",
+    "url": "/boulder-dash/static/css/3.9b169e42.chunk.css"
+  },
+  {
+    "revision": "c52a9f004d2d9085398d39600b3accf3",
+    "url": "/boulder-dash/index.html"
+  }
+];
